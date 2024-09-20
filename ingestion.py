366fea991072e7b640a7b4aae169f5b60dae3ed5
satvik nanda <satvik.nanda@gmail.com>
2024-09-20 16:41:38 +0530

    ingestion,splitting,embedding,storing

--- ingestion.py
+++ ingestion.py
@@ -10,8 +10,27 @@ load_dotenv()
 
 
 
-
-
-
 if __name__ == "__main__":
+    
+    #Loading the document
     print("Ingesting...")
+
+    loader = TextLoader("C:/Users/satvi/OneDrive/Desktop/langchain_medium/ingestion.py")
+    document = loader.load()
+
+    #splitting the document
+    print("Splitting...")
+    
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+
+    print(f"created {len(texts)} chunks")
+
+
+    #create embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+
+    #storing in pinecone
+    print("ingesting embeddings in pinecone")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.getenv("INDEX_NAME"))
